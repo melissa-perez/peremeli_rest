c3cb8db771ee5f79cb11b4231aad83e085504fb3
Melissa Perez <melmperz@gmail.com>
2021-11-29 23:32:43 -0500

    Add extra option

--- controller.mjs
+++ controller.mjs
@@ -9,7 +9,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json()).use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
