4439d8435745dfeaa2bc6a2ec291540df705b7db
Melissa Perez <melmperz@gmail.com>
2021-12-02 20:16:23 -0500

    Remove res from get

--- controller.mjs
+++ controller.mjs
@@ -44,7 +44,7 @@ app.post("/exercises", (req, res) => {
 /**
  * Retrieve all exercises.
  */
-app.get("/exercises", (req, res) => {
+app.get("/exercises", (res) => {
   exercises
     .findExercises({}, "", 0)
     .then((exercises) => {
